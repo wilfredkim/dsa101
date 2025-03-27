48f51eb2028401a01dcc98614a957bd2532991d7
Wilfred <wilfredkim5@gmail.com>
2025-03-27 23:30:09 +0300

    binary search tree

---
+++ src/main/java/com/wilfred/dsa/binarysearchtree/Main.java
@@ -0,0 +1,17 @@
+package com.wilfred.dsa.binarysearchtree;
+
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+       binarySearchTree.insert(10);
+       binarySearchTree.insert(1);
+       binarySearchTree.insert(3);
+       binarySearchTree.insert(12);
+       binarySearchTree.insert(76);
+       binarySearchTree.insert(100);
+       binarySearchTree.insert(55);
+       binarySearchTree.insert(43);
+        System.out.println(binarySearchTree.root.left.right.value);
+        System.out.println(binarySearchTree.contains(101));
+    }
+}
