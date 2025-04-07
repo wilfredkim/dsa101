4d598273f96df2e7ac55a9fea32c4774d24f0352
Wilfred <wilfredkim5@gmail.com>
2025-04-07 23:18:25 +0300

    heap

--- src/main/java/com/wilfred/dsa/graph/Graph.java
+++ src/main/java/com/wilfred/dsa/graph/Graph.java
@@ -275,4 +275,6 @@ public class Graph {
     static boolean isSafe(int i, int j, int n, int m) {
         return (i >= 0 && i < n && j >= 0 && j < m);
     }
+
+
 }
---
+++ src/main/java/com/wilfred/dsa/heap/Main.java
@@ -0,0 +1,15 @@
+package com.wilfred.dsa.heap;
+
+public class Main {
+    public static void main(String[] args) {
+        Heap myHeap = new Heap();
+        myHeap.insert(56);
+        myHeap.insert(99);
+        myHeap.insert(72);
+        myHeap.insert(61);
+        myHeap.insert(58);
+        System.out.println(myHeap.getHeap());
+        myHeap.insert(100);
+        System.out.println(myHeap.getHeap());
+    }
+}
